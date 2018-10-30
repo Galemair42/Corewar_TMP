4fd3c416e9609700d1042c546861fc2d1e59d5d0
Galemair42 <galemair@student.42.fr>
2018-10-30 19:42:26 +0100

    ok

--- vm/src/instruction/cw_get_instructions_params.c
+++ vm/src/instruction/cw_get_instructions_params.c
@@ -6,50 +6,82 @@
 /*   By: galemair <galemair@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/26 14:17:12 by galemair          #+#    #+#             */
-/*   Updated: 2018/10/26 17:41:45 by galemair         ###   ########.fr       */
+/*   Updated: 2018/10/30 19:40:32 by galemair         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include "corewar.h"
 //#include <stdlib.h>
+
 #include <stdio.h>
 /*
-**		return -1 when an error occurs
+**		Receive the OCP and the current PC
+**		First, it will isolate the 2 lightest bits to determine the size of the
+**		parameter
+**		Then it will read on the memory to calculate the value of the 
+**		parameters and will advance the current_PC
+**		Return -1 when an error occurs
+**		!! STILL HAVE TO MANAGE DIRECT PARAMETERS !! CARE IF IT STANDS FOR AN ADDRESS OR AN INTEGER	**
 */
-int		get_params(unsigned int ocp)
+unsigned int		get_size(unsigned int ocp)
 {
-	int m_register;
-	int	m_direct;
-	int m_index;
+	if (ocp == 1)
+		return (1);
+	if (ocp == 2)
+		return (4);
+	if (ocp == 3)
+		return (2);
+	return (0);
+}
+
+int		get_params(unsigned int ocp, unsigned int *current_pc)
+{
+	int	return_value;
+	int i;
+
+	i = 0;
+	value = 0;
 	printf("before %u\n", ocp);	
-	ocp = ocp << 30;
-	ocp = ocp >> 30;
+	ocp = ocp >> 30;	
 	printf("after %u\n", ocp);
-	m_register = 0x1;
-	m_direct = 0x2;
-	m_index = 0x3;
 	if (ocp == 0)
-	//ocp = ocp << 
+	{
+		printf("Parameters is non existant, how do we handle it ?\n");
+		exit(0);
+	}
+	while (i < get_size(ocp))
+	{
+		value += calculate_power(16, (ocp - i - 1)) * memory[0xFFF & (i + *current_pc)];
+		i++;
+	}
+	*current_pc += i;
+	return (value);
+}
+/*
+**		If an instruction has an OCP, this function is called
+**		It will read the params in the virtual memory and stock them in the variable 
+**		params of the structure t_processus
+*/
+int		get_params(unsigned int *tab, t_processus *process)
+{
+	unsigned int ocp;
+	int	i;
+	unsigned int current_pc;
+
+	i = 0;
+	current_pc = 0xFFF & (process->pc + 4);
+	ocp = process->ocp;
+	if (ocp > 255 || process.opc > 16)
+		return (-1);
+	ocp = opc << 24;//Met le bit interessant tout a gauche
+	while (i < op_tab[process->opc - 1])
+	{
+		tab[i] = get_params(ocp, &current_pc);
+		i++;
+		ocp = ocp << 2;
+	}
 	return (1);
 }
-//int		get_params(unsigned int *tab, t_processus *process)
-//{
-//	int opc_index;
-//	unsigned int ocp;
-//	int	i;
-//
-//	printf("%u", sizeof(int));
-//	i = 0;
-//	ocp = process->ocp;
-//	opc_index = process->opc - 1;
-//	if (ocp > 255 || process.opc > 16)
-//		return (-1);
-//	while (i < op_tab[opc_index])
-//	{
-//		ocp = ocp << 2;
-//	}
-//	return (0);
-//}
 //int		main(int argc, char **argv)
 //{
 //	t_processus processus;
@@ -58,8 +90,3 @@ int		get_params(unsigned int ocp)
 //	get_params(NULL, processus);
 //	return (0);
 //}
-int		main(int argc, char **argv)
-{
-	get_params(4);
-	return (0);
-}
